ead9886e40aca7390cbe2ed49a0cb3f1c8c5e0ae
bhwong11 <user@email.com>
2021-08-18 19:14:42 -0400

    added home view and rendering H1

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from django.views.generic import DetailView
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse
 
 # Create your views here.
+
+
+class Home(TemplateView):
+    template_name = 'home.html'
