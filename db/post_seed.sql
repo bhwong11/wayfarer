d3126730c3d81a163ea9fcf4bf1fe53239cfa798
aggarabedian <abrahamgarabedian@gmail.com>
2021-08-19 10:07:17 -0700

    Adds post_seed file

---
+++ db/post_seed.sql
@@ -0,0 +1,19 @@
+INSERT INTO main_app_post(title, content, image, created_at, users_id)
+  VALUES
+    ('An ancient Roman palace', 'This is a sample post.', 'https://cdn.britannica.com/68/95568-050-A0955C0F/Palace-of-Diocletian-Split-Croatia.jpg', '2021-08-19 9:45:45', '1');
+
+INSERT INTO main_app_post(title, content, image, created_at, users_id)
+  VALUES
+    ('Beautiful and spooky', 'This is a sample post.', 'http://www.catacombes.paris.fr/sites/default/files/styles/1920x1080/public/2018-10/cat-footer-faq.jpg?itok=_QDHeHmX', '2021-08-19 9:45:45', '1');
+
+INSERT INTO main_app_post(title, content, image, created_at, users_id)
+  VALUES
+    ('Just Like Gladiator', 'This is a sample post.', 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Colosseo_2020.jpg/1200px-Colosseo_2020.jpg', '2021-08-19 9:45:45', '1');
+
+INSERT INTO main_app_post(title, content, image, created_at, users_id)
+  VALUES
+    ('Remembering the Alamo', 'This is a sample post.', 'https://www.history.com/.image/t_share/MTU3ODc4NTk4NDEyNTQzMzA1/ask-alamo-survivor-istock_000024609914large-2.jpg', '2021-08-19 9:45:45', '1');
+
+INSERT INTO main_app_post(title, content, image, created_at, users_id)
+  VALUES
+    ('A step into the past', 'This is a sample post.', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Machu_Picchu%2C_Peru.jpg/1200px-Machu_Picchu%2C_Peru.jpg', '2021-08-19 9:45:45', '1');
