eda36a2c7df735ba8fdbd8b29746729bb257939d
bhwong11 <user@email.com>
2021-08-19 12:54:44 -0400

    add redirect for login

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.Home.as_view(),name='home'),
-    path('profile',views.Profile.as_view(),name='profile')
+    path('profile/',views.Profile.as_view(),name='profile')
 	# put urls here
 ]
