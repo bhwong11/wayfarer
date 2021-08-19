0dcae057cba68e1793637f5c1635de9bc63ce855
bhwong11 <user@email.com>
2021-08-19 15:30:38 -0400

    added public folder and new url paths, including mock home page for errors

---
+++ main_app/public/scripts/main.js
@@ -0,0 +1,8 @@
+$(document).ready( function() {
+    $(".sign-in").on("click", function() {
+        $(".modal-body").load("/accounts/login");
+    });
+    $(".sign-up").on("click", function() {
+        $(".modal-body").load("/accounts/signup");
+    });
+});
