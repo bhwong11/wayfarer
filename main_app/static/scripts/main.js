7a8e746156dac1d4c4368e5ebd0b53483f2589b6
bhwong11 <user@email.com>
2021-08-19 22:16:21 -0400

    added erro handling logic to scripts page, if invalid login or register error will appear on modal

--- main_app/static/scripts/main.js
+++ main_app/static/scripts/main.js
@@ -1,9 +1,54 @@
 $(document).ready( function() {
     $(".sign-in").on("click", function() {
         $(".modal-body").load("/accounts/login");
+        //start
+        $(document).on('submit','form',function(e){
+        e.preventDefault();
+        $.ajax({
+            type:'POST',
+            url:'/accounts/login/',
+            data:
+            {
+                username:$("#id_username").val(),
+                password:$("#id_password").val(),
+                csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val()
+            },
+            success:function(resultData){
+                  if(String(resultData).includes('Profile')){
+                    window.location.replace("/profile");
+                  }else{
+                  $(".modal-body").html(resultData)
+                    }
+                  }
+
+            })
+        });
+        //end
     });
     $(".sign-up").on("click", function() {
         $(".modal-body").load("/accounts/signup");
+        //start
+        $(document).on('submit','form',function(e){
+        e.preventDefault();
+        $.ajax({
+            type:'POST',
+            url:'/accounts/signup/',
+            data:
+            {
+                username:$("#id_username").val(),
+                password:$("#id_password1").val(),
+                password2:$("#id_password2").val(),
+                csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val()
+            },
+            success:function(resultData){
+                if(String(resultData).includes('Profile')){
+                    window.location.replace("/profile");
+                  }else{
+                  $(".modal-body").html(resultData)
+                    }
+                    }
+            })
+        });
+        //end
     });
 });
-
