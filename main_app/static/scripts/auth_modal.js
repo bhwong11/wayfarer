fab1502d5461e6c97440d0eacb441a7ae52dc9b1
bhwong11 <user@email.com>
2021-08-24 14:38:39 -0400

    added validation for create post edit post and ordered post

--- main_app/static/scripts/auth_modal.js
+++ main_app/static/scripts/auth_modal.js
@@ -1,75 +1,75 @@
-
-    let signIn = false
-    //signIn event handler
-    const signInHandler = function(e){
-        if(signIn){
-            e.preventDefault();
-            $.ajax({
-                type:'POST',
-                url:'/accounts/login/',
-                data:
-                {
-                    username:$("#id_username").val(),
-                    password:$("#id_password").val(),
-                    csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val()
-                },
-                success:function(resultData){
-                    $(".modal-body").html('')
-                      if(String(resultData).includes('Profile')){
-                        window.location.replace("/profile");
-                      }else{
-                    $(".modal-body").html('')
-                      $(".modal-body").html(resultData)
-                        }
-                      }
-    
-                })
-        }
-
-    }
-    $(".sign-in").on("click", function() {
-        signIn = true;
-        $(".modal-body").html('')
-        $(".modal-body").load("/accounts/login");
-        //start
-        //end
-    });
-
-    $('#exampleModal').on('submit','form',signInHandler);
-    
-
-    //sign up event handler
-    const signUpHandler = function(e){
-        if(!signIn){
-            e.preventDefault();
-            $.ajax({
-                type:'POST',
-                url:'/accounts/signup/',
-                data:
-                {
-                    username:$("#id_username").val(),
-                    password1:$("#id_password1").val(),
-                    password2:$("#id_password2").val(),
-                    csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val()
-                },
-                success:function(resultData){
-                    if(String(resultData).includes('Profile')){
-                        window.location.replace("/profileupdate");
-                      }else{
-                      $(".modal-body").html(resultData)
-                        }
-                        }
-                })
-        }
-    }
-    $(".sign-up").on("click", async function() {
-        signIn = false;
-        $(".modal-body").html('')
-        await $(".modal-body").load("/accounts/signup");
-    });
-
-    $('#exampleModal').on('submit','form',signUpHandler);
-    
-if(window.location.pathname==='/accounts/login/' || window.location.pathname==='/accounts/signup/'){
-    window.location.replace("/");
-}
+//
+//    let signIn = false
+//    //signIn event handler
+//    const signInHandler = function(e){
+//        if(signIn){
+//            e.preventDefault();
+//            $.ajax({
+//                type:'POST',
+//                url:'/accounts/login/',
+//                data:
+//                {
+//                    username:$("#id_username").val(),
+//                    password:$("#id_password").val(),
+//                    csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val()
+//                },
+//                success:function(resultData){
+//                    $(".modal-body").html('')
+//                      if(String(resultData).includes('Profile')){
+//                        window.location.replace("/profile");
+//                      }else{
+//                    $(".modal-body").html('')
+//                      $(".modal-body").html(resultData)
+//                        }
+//                      }
+//    
+//                })
+//        }
+//
+//    }
+//    $(".sign-in").on("click", function() {
+//        signIn = true;
+//        $(".modal-body").html('')
+//        $(".modal-body").load("/accounts/login");
+//        //start
+//        //end
+//    });
+//
+//    $('#exampleModal').on('submit','form',signInHandler);
+//    
+//
+//    //sign up event handler
+//    const signUpHandler = function(e){
+//        if(!signIn){
+//            e.preventDefault();
+//            $.ajax({
+//                type:'POST',
+//                url:'/accounts/signup/',
+//                data:
+//                {
+//                    username:$("#id_username").val(),
+//                    password1:$("#id_password1").val(),
+//                    password2:$("#id_password2").val(),
+//                    csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val()
+//                },
+//                success:function(resultData){
+//                    if(String(resultData).includes('Profile')){
+//                        window.location.replace("/profileupdate");
+//                      }else{
+//                      $(".modal-body").html(resultData)
+//                        }
+//                        }
+//                })
+//        }
+//    }
+//    $(".sign-up").on("click", async function() {
+//        signIn = false;
+//        $(".modal-body").html('')
+//        await $(".modal-body").load("/accounts/signup");
+//    });
+//
+//    $('#exampleModal').on('submit','form',signUpHandler);
+//    
+//if(window.location.pathname==='/accounts/login/' || window.location.pathname==='/accounts/signup/'){
+//    window.location.replace("/");
+//}
