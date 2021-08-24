4567a99b854998c5adc22f679ab32485475b032d
nickarredondo <Nicka721@gmail.com>
2021-08-23 17:54:20 -0700

    Config deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn wayfare_project.wsgi
--- wayfare_project/settings.py
+++ wayfare_project/settings.py
@@ -127,3 +127,8 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/profile/'
 LOGOUT_REDIRECT_URL = '/'
+
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
